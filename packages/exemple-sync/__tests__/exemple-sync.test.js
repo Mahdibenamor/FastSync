5278a58b061f328ff26024f0c6ae829df83ebbf0
Mahdibenamor <mahdi.benamor@supcom.tn>
2023-10-11 23:58:45 +0200

    remove unneeded code

--- packages/exemple-sync/__tests__/exemple-sync.test.js
+++
@@ -1,7 +0,0 @@
-'use strict';
-
-const exempleSync = require('..');
-const assert = require('assert').strict;
-
-assert.strictEqual(exempleSync(), 'Hello from exempleSync');
-console.info('exempleSync tests passed');
--- packages/exemple-sync/lib/exemple-sync.js
+++
@@ -1,7 +0,0 @@
-'use strict';
-
-module.exports = exempleSync;
-
-function exempleSync() {
-  return 'Hello from exempleSync';
-}

09c6d70b4d82db993c7330a108f55850a959169b
Mahdibenamor <mahdi.benamor@supcom.tn>
2023-12-22 16:13:49 +0100

    adding project structure

---
+++ fast_sync_dotnet_service/exemple/Program.cs
@@ -0,0 +1,25 @@
+
+using fast_sync_core.implementation;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+
+builder.Services.AddControllers();
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+var app = builder.Build();
+
+
+app.UseSwagger();
+app.UseSwaggerUI();
+
+app.UseHttpsRedirection();
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
